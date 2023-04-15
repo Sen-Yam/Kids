5ebf68cb34293fdbef14e6e45c45bcffa6efbadc
Sen-Yam <96739035+Sen-Yam@users.noreply.github.com>
2023-04-15 22:36:31 +0000

    done

---
+++ src/Vente.js
@@ -0,0 +1,16 @@
+import img1 from "./asset/imgCub.jpg"
+import img2 from "./asset/imgVoiture.jpg"
+export const products= [ {  
+    "id":1,
+     "type":"jeux de stratégie",
+     "age":"5ans",
+     "prix":"1000",
+     "image":img1
+  },
+   { 
+    "id":2,
+     "type":"jouet pour bébé",
+     "age":"moins de 2 ans",
+     "prix":"50da",
+     "image":img2
+ }]
--- src/pages/Vendre.js
+++ src/pages/Vendre.js
@@ -1,27 +1,39 @@
 import React, { useState, useEffect } from "react";
 import BannerJouets from "../components/BannerJouets";
 import "../style/Jouets.css";
-
+import {products} from "../Vente"
 const Vendre = () => {
   document.title = "jouets!";
-
+ 
   const [data, setData] = useState([]);
+  useEffect(() => {
+    setData(products)
+},);
+  /*
   useEffect(() => {
     fetch("../vente.json")
       .then((res) => res.json())
       .then((jeux) => setData(jeux))
       .catch((error) => console.error(error));
     console.log(data);
-  }, []);
+  }, []); */
   return (
     <div className="jouet">
       <BannerJouets />
       <h1 className="jouetTitle">Les articles jouets par tranche d'age</h1>
       <p className="jouetText">Des joués pour vos enfants</p>
-      {data && data.map(({ prix }, k) => <p key={k}>{prix}</p>)}
+      <div className="jeux" >
+    
       {data &&
-        data.map(({ image }, k) => <img key={k} src={image} alt="voiture" />)}
-    </div>
+        data.map((item, k) =><div className="element" key={k}>
+          <p>{item.prix}</p>
+          <img className="img"   src={item.image} alt="voiture" />
+
+
+        </div>)  }
+
+     </div>
+     </div>
   );
 };
 
